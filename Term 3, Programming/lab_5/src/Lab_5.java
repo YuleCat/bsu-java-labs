f0551ec410b48c231893e1d1854379ff7d7dd88f
Anna Nizhnikova <odrus.008@gmail.com>
2018-09-07 00:32:57 +0300

    code style improvements

--- Term 3, Programming/lab_5/src/Lab_5.java
+++ Term 3, Programming/lab_5/src/Lab_5.java
@@ -4,12 +4,8 @@ import java.text.NumberFormat;
  * Created by Yule Cat on 09.10.2017.
  */
 
-// variant 7
-// Horstman - Java
-
-
 public class Lab_5 {
-    private static double calc(double x, double eps){
+    private static double calc(double x, double eps) {
         double k = 1;
         double slag = Math.pow(x, 2 * k) / Math.pow(2, k) / k,
                 res = slag;
@@ -21,18 +17,18 @@ public class Lab_5 {
         return res;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         double x = 0, eps = 0;
-        try{
-            if (args.length != 2) throw new MyException("Error! Exactly 2 arguments required.");
+        try {
+            if (args.length != 2)
+                throw new MyException("Error! Exactly 2 arguments required.");
 
             x = Double.parseDouble(args[0]);
             eps = Double.parseDouble(args[1]);
             System.out.printf("Sum of the series = %.10f \n", calc(x, eps));
         }
-        catch (MyException | NumberFormatException ex){
+        catch (MyException | NumberFormatException ex) {
             System.out.println("exc = " + ex);
         }
-
     }
 }
