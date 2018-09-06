5230dd52fb1173063b4da2d82f7e7bb777a833ce
Anna Nizhnikova <odrus.008@gmail.com>
2018-09-07 00:33:32 +0300

    code style improvements

--- Term 3, Programming/lab_5/src/MyException.java
+++ Term 3, Programming/lab_5/src/MyException.java
@@ -1,8 +1,9 @@
 /**
  * Created by Yule Cat on 14.10.2017.
  */
+
 public class MyException extends Exception {
-    public MyException(String ex){
+    public MyException(String ex) {
         super(ex);
     }
 }
